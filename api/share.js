af408fdfc0e90798b58516f1921374a8e8e53116
Yoda <yoda@yodacode.xyz>
2023-01-08 04:21:00 -0800

    Attempt to support slack unfurling

--- api/share.js
+++ api/share.js
@@ -8,9 +8,11 @@ export default function handler (req, res) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="/assets/hc.css"></link>
-        <meta property="og:image" content="https://workshop-cards.hackclub.com/${encodeURIComponent(data.split('_').reverse()[0] + '\'s')}.png?theme=dark&fontSize=225px&caption=Bank%2520Wrapped%25202022&brand=Bank">
-        <meta property="og:title" content="Bank Wrapped">
-        <meta property="og:description" content="Watch ${data.split('_').reverse()[0].replace(/[\u00A0-\u9999<>\&]/g, ((i) => `&#${i.charCodeAt(0)};`))}'s Bank Wrapped for 2022">
+        <meta property="og:image" content="https://workshop-cards.hackclub.com/${encodeURIComponent(data.split('_').reverse()[0] + '\'s')}.png?theme=dark&fontSize=225px&caption=Bank%2520Wrapped%25202022&brand=Bank" />
+        <meta property="og:title" content="Bank Wrapped" />
+        <meta property="og:url" content="https://bank-wrapped.hackclub.com${req.url}" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Watch ${data.split('_').reverse()[0].replace(/[\u00A0-\u9999<>\&]/g, ((i) => `&#${i.charCodeAt(0)};`))}'s Bank Wrapped for 2022" />
         <meta name="twitter:card" content="summary_large_image">
         <meta name="theme-color" content="#ec3750">
         <title>Bank Wrapped</title>
