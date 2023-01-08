558c7f1e0d149747cb88385868f383b83658f40b
Yoda <yoda@yodacode.xyz>
2023-01-08 03:56:36 -0800

    An attempt to untangle the jungle of rewrites

--- api/image.js
+++ api/image.js
@@ -1,3 +1,3 @@
 export default function handler (req, res) {
-    res.redirect('https://workshop-cards.hackclub.com/Hack%20OC.png?theme=dark&fontSize=225px&caption=Bank%2520Wrapped%25202022&brand=Bank');
+    res.redirect(`https://workshop-cards.hackclub.com/${encodeURIComponent(req.query.name + '\'s')}.png?theme=dark&fontSize=225px&caption=Bank%2520Wrapped%25202022&brand=Bank`);
 }
